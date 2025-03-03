e9d4c07e31b72c8127a3911bfd7e46db9a814485
robjmcgibbon <robjmcgibbon@gmail.com>
2025-03-03 13:25:43 +0100

    Add list of plot colours

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #/home/mcgibbon/Documents/flamingo_website/venv/bin/python generate_publication_list.py
 /home/mcgibbon/Documents/flamingo_website/venv/bin/python make_webpage.py
-rm -r /home/mcgibbon/public_html/flamingo
+rm -rf /home/mcgibbon/public_html/flamingo
 mv build /home/mcgibbon/public_html/flamingo
